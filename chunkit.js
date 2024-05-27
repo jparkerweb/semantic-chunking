964445dfd9f58d40139221448fad234aa41cd6ed
Justin Parker <jparkerweb@gmail.com>
2024-05-26 20:08:28 -0400

    add cramit function

--- chunkit.js
+++ chunkit.js
@@ -57,65 +57,102 @@ export async function chunkit(
         onnxEmbeddingModelQuantized = ONNX_EMBEDDING_MODEL_QUANTIZED,
     } = {}) {
 
-        // Load the tokenizer
-        tokenizer = await AutoTokenizer.from_pretrained(onnxEmbeddingModel);
+    // Load the tokenizer
+    tokenizer = await AutoTokenizer.from_pretrained(onnxEmbeddingModel);
 
-        // Create the embedding pipeline
-        generateEmbedding = await pipeline('feature-extraction', onnxEmbeddingModel, {
-            quantized: onnxEmbeddingModelQuantized,
-        });
+    // Create the embedding pipeline
+    generateEmbedding = await pipeline('feature-extraction', onnxEmbeddingModel, {
+        quantized: onnxEmbeddingModelQuantized,
+    });
 
-        // Split the text into sentences
-        const sentences = sentencize(text);
+    // Split the text into sentences
+    const sentences = sentencize(text);
 
-        // Compute the similarities between sentences
-        const { similarities, average, variance } = await computeAdvancedSimilarities(
-            sentences,
-            { 
-                numSimilaritySentencesLookahead: numSimilaritySentencesLookahead,
-                logging: logging,
-            }
-        );
-
-        // Dynamically adjust the similarity threshold based on variance and average
-        let dynamicThreshold = similarityThreshold;
-        if (average != null && variance != null) {
-            dynamicThreshold = adjustThreshold(average, variance, similarityThreshold, dynamicThresholdLowerBound, dynamicThresholdUpperBound );
+    // Compute the similarities between sentences
+    const { similarities, average, variance } = await computeAdvancedSimilarities(
+        sentences,
+        {
+            numSimilaritySentencesLookahead: numSimilaritySentencesLookahead,
+            logging: logging,
         }
+    );
 
-        // Create the initial chunks using the adjusted threshold
-        const initialChunks = createChunks(sentences, similarities, maxTokenSize, dynamicThreshold, logging);
+    // Dynamically adjust the similarity threshold based on variance and average
+    let dynamicThreshold = similarityThreshold;
+    if (average != null && variance != null) {
+        dynamicThreshold = adjustThreshold(average, variance, similarityThreshold, dynamicThresholdLowerBound, dynamicThresholdUpperBound);
+    }
 
+    // Create the initial chunks using the adjusted threshold
+    const initialChunks = createChunks(sentences, similarities, maxTokenSize, dynamicThreshold, logging);
+
+    if (logging) {
+        console.log('\n=============\ninitialChunks\n=============');
+        initialChunks.forEach((chunk, index) => {
+            console.log("\n");
+            console.log(`--------------`);
+            console.log(`-- Chunk ${(index + 1)} --`);
+            console.log(`--------------`);
+            console.log(chunk);
+        });
+    }
+
+    // // combine similar chunks and balance sizes
+    if (combineChunks) {
+        const combinedChunks = await optimizeAndRebalanceChunks(initialChunks, tokenizer, maxTokenSize, combineChunksSimilarityThreshold);
         if (logging) {
-            console.log('\n=============\ninitialChunks\n=============');
-            initialChunks.forEach((chunk, index) => {
-                console.log("\n");
-                console.log(`--------------`);
-                console.log(`-- Chunk ${(index + 1)} --`);
-                console.log(`--------------`);
+            console.log('\n\n=============\ncombinedChunks\n=============');
+            combinedChunks.forEach((chunk, index) => {
+                console.log("\n\n\n");
+                console.log("--------------------");
+                console.log("Chunk " + (index + 1));
+                console.log("--------------------");
                 console.log(chunk);
             });
         }
+        // Return the combined chunks
+        return combinedChunks;
+    } else {
+        // Return the initial chunks
+        return initialChunks;
+    }
+}
 
-        // // combine similar chunks and balance sizes
-        if (combineChunks) {
-            const combinedChunks = await optimizeAndRebalanceChunks(initialChunks, tokenizer, maxTokenSize, combineChunksSimilarityThreshold);
-            if (logging) { 
-                console.log('\n\n=============\ncombinedChunks\n=============');
-                combinedChunks.forEach((chunk, index) => {
-                    console.log("\n\n\n");
-                    console.log("--------------------");
-                    console.log("Chunk " + (index + 1));
-                    console.log("--------------------");
-                    console.log(chunk);
-                });
-            }
-            // Return the combined chunks
-            return combinedChunks;
-        } else {
-            // Return the initial chunks
-            return initialChunks;
-        }
+
+// --------------------------
+// -- Main cramit function --
+// --------------------------
+export async function cramit(
+    text,
+    {
+        logging = LOGGING,
+        maxTokenSize = MAX_TOKEN_SIZE,
+        onnxEmbeddingModel = ONNX_EMBEDDING_MODEL,
+        onnxEmbeddingModelQuantized = ONNX_EMBEDDING_MODEL_QUANTIZED,
+    } = {}) {
+
+    // Load the tokenizer
+    tokenizer = await AutoTokenizer.from_pretrained(onnxEmbeddingModel);
+
+    // Split the text into sentences
+    const sentences = sentencize(text);
+
+    // Create chunks
+    const chunks = createChunks(sentences, null, maxTokenSize, 0, logging);
+
+    if (logging) {
+        console.log('\n=============\nChunks\n=============');
+        chunks.forEach((chunk, index) => {
+            console.log("\n");
+            console.log(`--------------`);
+            console.log(`-- Chunk ${(index + 1)} --`);
+            console.log(`--------------`);
+            console.log(chunk);
+        });
+    }
+
+    // Return chunks
+    return chunks;
 }
 
 
@@ -148,8 +185,8 @@ async function computeSimpleSimilarities(sentences) {
     let similarities = [];
 
     for (let i = 0; i < embeddings.length - 1; i++) {
-      const sim = cosineSimilarity(embeddings[i], embeddings[i + 1]);
-      similarities.push(sim);
+        const sim = cosineSimilarity(embeddings[i], embeddings[i + 1]);
+        similarities.push(sim);
     }
 
     return similarities;
@@ -161,14 +198,14 @@ async function computeSimpleSimilarities(sentences) {
 // ---------------------------------------------------------------
 async function computeAdvancedSimilarities(sentences, { numSimilaritySentencesLookahead = 2, logging = false } = {}) {
     if (logging) console.log('numSimilaritySentencesLookahead', numSimilaritySentencesLookahead);
-    
+
     const embeddings = await Promise.all(sentences.map(sentence => createEmbedding(sentence)));
     let similarities = [];
     let similaritySum = 0;
 
     for (let i = 0; i < embeddings.length - 1; i++) {
         let maxSimilarity = cosineSimilarity(embeddings[i], embeddings[i + 1]);
-        
+
         for (let j = i + 2; j <= i + numSimilaritySentencesLookahead && j < embeddings.length; j++) {
             const sim = cosineSimilarity(embeddings[i], embeddings[j]);
             maxSimilarity = Math.max(maxSimilarity, sim);
@@ -222,20 +259,20 @@ function cosineSimilarity(vecA, vecB) {
     let dotProduct = 0.0;
     let normA = 0.0;
     let normB = 0.0;
-    
+
     for (let i = 0; i < vecA.length; i++) {
-      dotProduct += vecA[i] * vecB[i];
-      normA += vecA[i] ** 2;
-      normB += vecB[i] ** 2;
+        dotProduct += vecA[i] * vecB[i];
+        normA += vecA[i] ** 2;
+        normB += vecB[i] ** 2;
     }
-    
+
     normA = Math.sqrt(normA);
     normB = Math.sqrt(normB);
-    
+
     if (normA === 0 || normB === 0) {
-      return 0; // To avoid division by zero
+        return 0; // To avoid division by zero
     } else {
-      return dotProduct / (normA * normB);
+        return dotProduct / (normA * normB);
     }
 }
 
@@ -250,40 +287,59 @@ function createChunks(sentences, similarities, maxTokenSize, similarityThreshold
     let sentenceTokenCount;
 
     if (logging) { console.log(`!! new chunk !! --> 1`) }
-    
+
     for (let i = 1; i < sentences.length; i++) {
-      currentChunkSize = tokenizer(currentChunk.join(" ")).input_ids.size;
-      sentenceTokenCount = tokenizer(sentences[i]).input_ids.size;
+        currentChunkSize = tokenizer(currentChunk.join(" ")).input_ids.size;
+        sentenceTokenCount = tokenizer(sentences[i]).input_ids.size;
 
-      if (logging) { 
-        console.log('sentenceTokenCount', sentenceTokenCount);
-        console.log('currentChunkSize', currentChunkSize);
-        console.log('maxTokenSize', maxTokenSize)
-        console.log('similarity', similarities[i - 1])
-        console.log('similarityThreshold', similarityThreshold)
-      }
+        if (logging) {
+            console.log('sentenceTokenCount', sentenceTokenCount);
+            console.log('currentChunkSize', currentChunkSize);
+            console.log('maxTokenSize', maxTokenSize);
+            if (similarities) {
+                console.log('similarity', similarities[i - 1])
+                console.log('similarityThreshold', similarityThreshold)
+            }
+        }
 
-      if (similarities[i - 1] >= similarityThreshold && currentChunkSize  + sentenceTokenCount <= maxTokenSize) {
-          currentChunk.push(sentences[i]);
+        if (similarities) {
+            if (similarities[i - 1] >= similarityThreshold && currentChunkSize + sentenceTokenCount <= maxTokenSize) {
+                currentChunk.push(sentences[i]);
 
-          if (logging) { console.log('keep going...') }
-      } else {
-          chunks.push(currentChunk.join(" "));
-          currentChunk = [sentences[i]];
+                if (logging) { console.log('keep going...') }
+            } else {
+                chunks.push(currentChunk.join(" "));
+                currentChunk = [sentences[i]];
 
-          if (logging) { 
-            console.log('stop...')
-            console.log('\n')
-            console.log(`!! new chunk !! --> ${chunks.length + 1}`)
-          }
-      }
+                if (logging) {
+                    console.log('stop...')
+                    console.log('\n')
+                    console.log(`!! new chunk !! --> ${chunks.length + 1}`)
+                }
+            }
+        } else {
+            if (currentChunkSize + sentenceTokenCount <= maxTokenSize) {
+                currentChunk.push(sentences[i]);
+
+                if (logging) { console.log('keep going...') }
+            } else {
+                chunks.push(currentChunk.join(" "));
+                currentChunk = [sentences[i]];
+
+                if (logging) {
+                    console.log('stop...')
+                    console.log('\n')
+                    console.log(`!! new chunk !! --> ${chunks.length + 1}`)
+                }
+            }
+        }
     }
-  
+
     // Add the last chunk if it's not empty
     if (currentChunk.length > 0 && currentChunk[0] !== "") {
-      chunks.push(currentChunk.join(" "));
+        chunks.push(currentChunk.join(" "));
     }
-  
+
     return chunks;
 }
 
@@ -304,7 +360,7 @@ async function optimizeAndRebalanceChunks(combinedChunks, tokenizer, maxTokenSiz
         if (currentChunkText && (currentChunkTokenCount + chunkTokenCount <= maxTokenSize)) {
             const nextEmbedding = await createEmbedding(chunk);
             const similarity = currentEmbedding ? cosineSimilarity(currentEmbedding, nextEmbedding) : 0;
-            
+
             if (similarity >= combineChunksSimilarityThreshold) {
                 currentChunkText += " " + chunk;
                 currentChunkTokenCount += chunkTokenCount;
